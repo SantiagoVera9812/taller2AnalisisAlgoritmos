80359c38611a65b7165b38b5acc9ddb559cf1348
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2024-02-20 22:07:12 -0500

    Funcion recursiva terminada

--- src/main/java/com/taller/EsMitadImparRecursivo.java
+++ src/main/java/com/taller/EsMitadImparRecursivo.java
@@ -5,11 +5,27 @@ import java.util.List;
 public class EsMitadImparRecursivo {
 
     public static boolean esMitadImpar(List<Integer> numeros) {
-        return esMitadImparRecursivo(numeros, 0, numeros.size());
+        int inicio = 0;
+        int fin = numeros.size();
+        int mitad = (inicio + fin) / 2;
+
+        int countOdd = esMitadImparRecursivo(numeros, inicio, fin - 1, 0);
+        
+        return countOdd >= mitad;
     }
 
-    private static boolean esMitadImparRecursivo(List<Integer> numeros, int inicio, int fin) {
-      return false;
+    private static int esMitadImparRecursivo(List<Integer> numeros, int inicio, int fin, int count) {
+        
+        if (inicio > fin) {
+            return count;
+        } else {
+            
+            return esMitadImparRecursivo(numeros, inicio + 1, fin, count + isOdd(numeros.get(inicio)));
+        }
+}
+
+    private static int isOdd(int num) {
+        return num % 2 == 1 ? 1 : 0;
+
     }
-    
 }
